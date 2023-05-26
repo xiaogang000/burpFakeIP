95a0b91aa9a495a6c73885a5d7fa4cc91706e345
XG小刚 <50757673+xiaogang000@users.noreply.github.com>
2023-05-26 16:33:54 +0800

    Update BurpExtender.java

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -6,16 +6,10 @@ import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.PrintWriter;
+import java.util.Random;
 
-/**
- * Project: fakeIP
- * Date:2021/5/21 上午11:07
- *
- * @author CoolCat
- * @version 1.0.0
- * Github:https://github.com/TheKingOfDuck
- * When I wirting my code, only God and I know what it does. After a while, only God knows.
- */
+
 public class BurpExtender implements IBurpExtender, IContextMenuFactory, IIntruderPayloadGeneratorFactory, IIntruderPayloadGenerator, IHttpListener {
     public static IExtensionHelpers helpers;
     private String PLUGIN_NAME = "burpFakeIP";
@@ -24,9 +18,12 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, IIntrud
 
     @Override
     public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks) {
+        //通过 callbacks 这个实例对象，传递给插件一系列burp的原生方法。我们需要实现的很多功能都需要调用这些方法。
         helpers = callbacks.getHelpers();
 
+        // 获取burp提供的标准输出流和错误输出流
         stdout = new PrintWriter(callbacks.getStdout(), true);
+        //PrintWriter stderr = new PrintWriter(callbacks.getStderr(), true);
         String banner = "[+] %s %s is loaded...\n" +
                 "[+] ####################################\n" +
                 "[+]    Anthor: CoolCat\n" +
@@ -34,13 +31,16 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, IIntrud
                 "[+]    Github: https://github.com/TheKingOfDuck\n" +
                 "[+] ####################################\n" +
                 "[+] Enjoy it~";
+        // 打印到标准输出流
         stdout.println(String.format(banner, PLUGIN_NAME, VERSION));
+        stdout.println("Hello output");
 
         //注册菜单
         callbacks.registerContextMenuFactory(this);
         callbacks.registerIntruderPayloadGeneratorFactory(this);
-        callbacks.setExtensionName(PLUGIN_NAME);
         callbacks.registerHttpListener(this);
+        // 设置插件的名称
+        callbacks.setExtensionName(PLUGIN_NAME);
 
     }
 
@@ -53,11 +53,13 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, IIntrud
         JMenuItem localhost = new JMenuItem("127.0.0.1");
         JMenuItem random = new JMenuItem("randomIP");
         JMenuItem autoXFF = new JMenuItem("AutoXFF");
+        JMenuItem autoUA = new JMenuItem("AutoUA");
 
         menu.add(custom);
         menu.add(localhost);
         menu.add(random);
         menu.add(autoXFF);
+        menu.add(autoUA);
 
         if (iContextMenuInvocation.getInvocationContext() != IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST) {
             return menus;
@@ -106,7 +108,26 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, IIntrud
                 }
             }
         });
+        autoUA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
 
+                Object[] options = {"OFF", "ON"};
+                int flag = JOptionPane.showOptionDialog(null, "AutoUA Status: " + Config.AUTOUA_STAT, "FakeUA", JOptionPane.YES_OPTION, JOptionPane.PLAIN_MESSAGE,
+
+                        null, options, options[options.length - 1]);
+                switch (flag) {
+                    case 0:
+                        Config.AUTOUA_STAT = false;
+                        break;
+                    case 1:
+                        Config.AUTOUA_VALUE = JOptionPane.showInputDialog("Pls input ur UA header value:", Config.AUTOUA_VALUE);
+                        Config.AUTOUA_STAT = true;
+                        break;
+                    default:
+                }
+            }
+        });
         menus.add(menu);
         return menus;
     }
@@ -145,6 +166,15 @@ public class BurpExtender implements IBurpExtender, IContextMenuFactory, IIntrud
                 Utils.addfakeip(iHttpRequestResponse, Utils.getRandomIp());
             } else {
                 Utils.addfakeip(iHttpRequestResponse, Config.AUTOXFF_VALUE);
+            }
+        }
+        if (b && Config.AUTOUA_STAT) {
+            if (Config.AUTOUA_VALUE.equals("$RandomUA$")) {
+                Random rand = new Random();
+                String ua = Config.UA_LIST.get(rand.nextInt(Config.UA_LIST.size()));
+                Utils.addfakeua(iHttpRequestResponse, ua);
+            } else {
+                Utils.addfakeua(iHttpRequestResponse, Config.AUTOUA_VALUE);
             }
         }
     }
